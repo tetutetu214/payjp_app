8aa4debbdd55384f4a2ed41ef84328f4c2e7c4cd
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-16 14:19:08 +0900

    C　紐付くカード情報を取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
+    card = Card.find_by(user_id: current_user.id) # ユーザーのid情報を元に、カード情報を取得
   end
   
   def update
